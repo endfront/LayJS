8f567849505616a540dceebcbd55f42e28c27b93
relfor <me@relfor.co>
2015-12-18 23:00:29 -0800

    update version

--- LAY.js
+++ LAY.js
@@ -12,7 +12,7 @@
     // version is a method in order
     // to maintain the consistency of
     // only method accesses from the user
-    version: function(){ return "beta"; },
+    version: function(){ return "0.9.1"; },
 
     $pathName2level: {},
     $newlyInstalledStateLevelS: [],
